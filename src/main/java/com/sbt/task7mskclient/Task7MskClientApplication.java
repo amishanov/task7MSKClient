9ea17d17e0a22cff35e86a8502b564c13420d3b7
Мишанов Алексей Борисович <ABoMishanov@sberbank.ru>
2023-11-10 08:44:05 +0300

    Initial commit

---
+++ src/main/java/com/sbt/task7mskclient/Task7MskClientApplication.java
@@ -0,0 +1,13 @@
+package com.sbt.task7mskclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task7MskClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task7MskClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbt/task7mskclient/Task7MskClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbt.task7mskclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task7MskClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
